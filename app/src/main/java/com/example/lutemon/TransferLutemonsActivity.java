d7ff5eda35338c60d49a79d4512cd95612d0ae7e
Anne-Maria Keränen <anmarkeranen@gmail.com>
2023-04-05 16:43:50 +0300

    Added tabs for Lutemon transfer

--- app/src/main/java/com/example/lutemon/TransferLutemonsActivity.java
+++ app/src/main/java/com/example/lutemon/TransferLutemonsActivity.java
@@ -1,14 +1,43 @@
 package com.example.lutemon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class TransferLutemonsActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
+    ViewPagerAdapter viewPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer_lutemons);
+
+        tabLayout = findViewById(R.id.tlFields);
+        viewPager = findViewById(R.id.vpTabs);
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
