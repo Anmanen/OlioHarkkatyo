03bcff191c461026f06b08dfa5324608072ac42a
Anne-Maria Keränen <anmarkeranen@gmail.com>
2023-04-06 09:49:36 +0300

    Home fragment layout

--- app/src/main/java/com/example/lutemon/fragments/HomeFragment.java
+++ app/src/main/java/com/example/lutemon/fragments/HomeFragment.java
@@ -2,13 +2,20 @@ package com.example.lutemon.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import com.example.lutemon.R;
+import com.example.lutemon.domain.Lutemon;
+import com.example.lutemon.domain.Storage;
+
+import java.util.concurrent.Callable;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,14 +24,7 @@ import com.example.lutemon.R;
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private LinearLayout linearLayout;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -35,7 +35,18 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        linearLayout = view.findViewById(R.id.llHome);
+        Storage.getInstance().getLutemons().forEach((id, lutemon) -> linearLayout.addView(makeCheckbox(id, lutemon)));
+
+
+        return view;
+    }
+
+    public CheckBox makeCheckbox(int id, Lutemon lutemon){
+        CheckBox checkBox = new CheckBox(getActivity());
+        checkBox.setId(id);
+        checkBox.setText(lutemon.getName() + " " + lutemon.getColor());
+        return checkBox;
     }
 }
